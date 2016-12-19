6b259354577c7544979e0c3a61ccff53ec7a62ca
Rogerio-PC <Rogerio-PC@DESKTOP-T0FHA93.Home>
2016-12-19 14:44:19 -0200

    Finalizado tela de cadastro e pesquisa, aplicado bd mysql.

--- Documents/spring-cobranca-titulos/cobranca/src/main/java/com/rfaguiar/cobranca/CobrancaApplication.java
+++ Documents/spring-cobranca-titulos/cobranca/src/main/java/com/rfaguiar/cobranca/CobrancaApplication.java
@@ -1,7 +1,15 @@
 package com.rfaguiar.cobranca;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 @SpringBootApplication
 public class CobrancaApplication {
@@ -9,4 +17,19 @@ public class CobrancaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CobrancaApplication.class, args);
 	}
+	
+	@Bean
+	public LocaleResolver localResolver(){
+		return new FixedLocaleResolver(new Locale("pt", "BR"));
+	}
+	
+	@Configuration
+	public static class MvcConfig extends WebMvcConfigurerAdapter{
+		
+		@Override
+		public void addViewControllers(ViewControllerRegistry registry) {
+			registry.addRedirectViewController("/", "/titulos");
+		}
+	}
+	
 }
---
+++ Documents/spring-cobranca-titulos/cobranca/src/main/java/com/rfaguiar/cobranca/model/StatusTitulo.java
@@ -0,0 +1,17 @@
+package com.rfaguiar.cobranca.model;
+
+public enum StatusTitulo {
+
+	PENDENTE("Pendente"),
+	RECEBIDO("Recebido");
+	
+	private String descricao;
+
+	private StatusTitulo(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+}
